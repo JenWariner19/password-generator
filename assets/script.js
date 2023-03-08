d0e73c1baf1a78bb8ab0dce90f2613f005191179
Jen Wariner <jenniferwariner@yahoo.com>
2023-03-08 13:14:56 -0600

    added length parameters question

--- assets/script.js
+++ assets/script.js
@@ -4,20 +4,26 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
 generateBtn.addEventListener("click", function() {
   confirm("Do you need uppercase letters?");
   confirm("Do you need lowercase letters?");
   confirm("Do you need numbers?");
   confirm("Do you need uppercase symbols?");
-});
+
+  var length = prompt("Choose the length of your password from 8-128 characters.")
+
+  if (length <= 8 && length >= 128) {
+    alert("You must choose a number between 8 - 128.")
+    return;
+  }
+  else {
+    alert("Your selections have been recorded.")
+    return;
+  }}
+);
 
 // Write password to the #password input
-
-//function generatePassword() {
- // if (generateBtn) {
- //   alert("Do you want uppercase letters?");
-//  };
-//}
 
 function writePassword() {
   var password = generatePassword();
